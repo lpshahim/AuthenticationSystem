f69abc2a259c940f4b8515b5493491368ff27b97
lpshahim <lp.shahim6@gmail.com>
2017-08-22 14:33:22 +0200

    Changes Made to Project

--- AuthenticationSystem/Form1.Designer.cs
+++ AuthenticationSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AuthenticationSystem
 {
-    partial class Form1
+    partial class AuthenticationForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // AuthenticationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "AuthenticationForm";
+            this.Text = "Authentication System";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- AuthenticationSystem/Form1.cs
+++ AuthenticationSystem/Form1.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace AuthenticationSystem
 {
-    public partial class Form1 : Form
+    public partial class AuthenticationForm : Form
     {
-        public Form1()
+        public AuthenticationForm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load (object sender, EventArgs e)
         {
            
         }
