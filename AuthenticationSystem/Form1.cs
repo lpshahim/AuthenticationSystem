f2970c574b39607a469db8dac5abef98ed75745f
lpshahim <lp.shahim6@gmail.com>
2017-08-22 14:28:04 +0200

    Test

--- AuthenticationSystem/Form1.cs
+++ AuthenticationSystem/Form1.cs
@@ -19,7 +19,7 @@ namespace AuthenticationSystem
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world");
+           
         }
     }
 }
